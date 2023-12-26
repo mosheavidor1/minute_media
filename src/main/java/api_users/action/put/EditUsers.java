001fc6e058c230b3db8e04499ae0707697adc523
mosheavidor1 <mosheavidorster@gmail.com>
2023-12-26 03:05:15 +0200

    PUT code changes

--- src/main/java/api_users/action/put/EditUsers.java
+++ src/main/java/api_users/action/put/EditUsers.java
@@ -1,11 +1,12 @@
 
+
+
 package api_users.action.put;
 
 import api_users.infra.api.ApiRequestsPage;
 import api_users.infra.api.ApiStrings;
 import api_users.infra.api.messages.ResponseHandler;
 import com.google.gson.Gson;
-
 import java.net.URI;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
@@ -14,32 +15,61 @@ import java.util.Map;
 public class EditUsers {
 
 public static void editUserFunction() {
-    try {
-        ApiStrings apiStrings = new ApiStrings();
+try {
+    ApiStrings apiStrings = new ApiStrings();
 
-        String editedId = apiStrings.setUserID("55");
+    String editedId = apiStrings.setUserID("5");
 
-        URI uri = new URI(ApiStrings.URI + ApiStrings.PORT + "/" + ApiStrings.USERS_PATH + "/"
-                + editedId);
+    URI uri = new URI(ApiStrings.URI + ApiStrings.PORT + "/" + ApiStrings.USERS_PATH + "/" + editedId);
 
-        String updatedUserName = apiStrings.setUserName("Mike");
+    String updatedUserName = apiStrings.setUserName("Mike");
 
-        Map<String, String> jsonBody = new HashMap<>();
-        jsonBody.put(ApiStrings.NAME, updatedUserName);
-        jsonBody.put(ApiStrings.ID, editedId);
+    Map<String, String> jsonBody = new HashMap<>();
+    jsonBody.put(ApiStrings.NAME, updatedUserName);
+    jsonBody.put(ApiStrings.ID, editedId);
 
-        Gson gson = new Gson();
-        String requestBody = gson.toJson(jsonBody);
+    Gson gson = new Gson();
+    String requestBody = gson.toJson(jsonBody);
 
-        ApiRequestsPage apiRequestsPage = new ApiRequestsPage();
+    ApiRequestsPage apiRequestsPage = new ApiRequestsPage();
 
-        HttpResponse<String> response = apiRequestsPage.performPutRequest(uri, requestBody);
+    HttpResponse<String> response = apiRequestsPage.performPutRequest(uri, requestBody);
 
-        ResponseHandler.handleResponse(response);
+    ResponseHandler.handleResponse(response);
 
-    } catch (Exception e) {
-        e.printStackTrace();
+
+    checkUserDetails(updatedUserName, editedId);
+
+} catch (Exception e) {
+    e.printStackTrace();
+}
+}
+
+private static void checkUserDetails(String expectedUserName, String userId) {
+try {
+    ApiStrings apiStrings = new ApiStrings();
+
+    URI uri = new URI(ApiStrings.URI + ApiStrings.PORT + "/" + ApiStrings.USERS_PATH + "/" + userId);
+
+    ApiRequestsPage apiRequestsPage = new ApiRequestsPage();
+
+    HttpResponse<String> response = apiRequestsPage.performGetRequest(uri);
+
+    String responseBody = response.body();
+
+    Gson gson = new Gson();
+    Map<String, String> userDetails = gson.fromJson(responseBody, HashMap.class);
+
+    String actualUserName = userDetails.get(ApiStrings.NAME);
+    if (actualUserName.equals(expectedUserName)) {
+        System.out.println("User name was updated successfully: " + actualUserName);
+    } else {
+        System.out.println("User name update failed!, name is not equals to the new edited name ");
+
     }
+} catch (Exception e) {
+    e.printStackTrace();
+}
 }
 }
 
